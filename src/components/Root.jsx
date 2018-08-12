d66ba9ea3555ff5cad135e8ea687b64d7bff18e0
A_Yoneda <abesi5991@gmail.com>
2018-08-13 01:47:20 +0900

    Router

---
+++ src/components/Root.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import App from './App.jsx';
+import AppChat from './AppChat.jsx';
+import Profile from './Profile.jsx';
+
+
+const Root = () => (
+  <BrowserRouter>
+    <div>
+      <ul>
+        <li><Link to="/">App</Link></li>
+        <li><Link to="/chat">AppChat</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+      </ul>
+      <hr />
+      <Route exact path="/" component={App} />
+      <Route path="/chat" component={AppChat} />
+      <Route path="/profile" component={Profile} />
+    </div>
+  </BrowserRouter>
+);
+
+export default Root;
--- src/index.js
+++ src/index.js
@@ -2,24 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-// Login
-import App from './components/App.jsx';
-// Chat
-// import AppChat from './components/AppChat.jsx';
-// Profile
-// import ProfileAppearance from './components/profile.jsx';
+import Root from './components/Root.jsx';
 
 // キャッシュを残す? 本実装まではコメントアウト
 // import registerServiceWorker from './registerServiceWorker';
 
-// Login
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// Chat
-// ReactDOM.render(<AppChat />, document.getElementById('root'));
-
-// Profile
-// ReactDOM.render(<ProfileAppearance />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // キャッシュを残す? 本実装まではコメントアウト
 // registerServiceWorker();
