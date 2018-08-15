7b6645442c706722fccf10bd05c663b0dc1c107f
A_Yoneda <abesi5991@gmail.com>
2018-08-15 21:41:01 +0900

    投稿日時とチャットの取得関係を変更 notifを実装中途でコメントアウトして放棄

--- src/components/AppChat.jsx
+++ src/components/AppChat.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import '../css/App.css';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 import { theme } from './configs/mui/config.jsx';
 
 import { firestore, FieldValue } from './configs/firebase/config.jsx';
@@ -11,7 +13,7 @@ import ChatBox from './ChatBox.jsx';
 // firebaseStoreのchatlogにアクセス
 const messagesCol = firestore.collection('chatlog');
 // firebaseStoreの監視を終了させるやつ
-const unsubscribe = messagesCol.onSnapshot(() => {});
+let unsubscribe;
 
 class AppChat extends Component {
   constructor(props) {
@@ -32,29 +34,78 @@ class AppChat extends Component {
   // Mountされた時実行
   componentWillMount() {
     // timestampで並び替えて50件まで表示
-    messagesCol.orderBy('timestamp').limit(50).onSnapshot((snapshot) => {
-      snapshot.docChanges().forEach((change) => {
+    unsubscribe = messagesCol.orderBy('timestamp', 'desc').limit(2).onSnapshot((snapshot) => {
+      snapshot.docChanges().reverse().forEach((change) => {
         const msgs = this.state.messages;
         // 追加時
+        // 削除等で配列数が変動し、limitに収まる配列が増えたときもadd扱いになるのをどうにかする
         if (change.type === 'added') {
+          console.log('added');
+
+          const time = change.doc.data().timestamp;
+          // timestampが確定してるもののみ通す
+          if (time !== null) {
+            const timeText = `${time.getMonth() + 1}/${time.getDate()} ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
+            msgs.push({
+              id: change.doc.id,
+              text: change.doc.data().text,
+              user_name: change.doc.data().user_name,
+              profile_image: change.doc.data().profile_image,
+              timestamp: timeText,
+            });
+            this.setState({
+              messages: msgs,
+            });
+          }
+        } else if (change.type === 'modified') {
+          // 変更された時
+          console.log('modified');
+
+          const time = change.doc.data().timestamp;
+          const timeText = `${time.getMonth() + 1}/${time.getDate()} ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
           msgs.push({
+            id: change.doc.id,
             text: change.doc.data().text,
             user_name: change.doc.data().user_name,
             profile_image: change.doc.data().profile_image,
+            timestamp: timeText,
           });
-          const temp = msgs;
           this.setState({
-            messages: temp,
+            messages: msgs,
           });
-        }
-        if (change.type === 'modified') {
-          // 変更された時
-        }
-        if (change.type === 'removed') {
+        } else if (change.type === 'removed') {
           // 削除された時
+          console.log('removed');
+
+          const target = change.doc.id;
+          msgs.some((msg, i) => {
+            if (msg.id === target) {
+              msgs.splice(i, 1);
+            }
+            return false;
+          });
+          this.setState({
+            messages: msgs,
+          });
         }
       });
     });
+    // const notif = window.Notification;
+    // window.Notification.requestPermission()
+    // if (!('Notification' in window)) {
+    //   alert('無理無理かたつむり');
+    // } else if (notif.permission === 'granted') {
+    //   const notification = new window.Notification('Hi there');
+    // } else if (notif.permission === 'denied') {
+    //   console.log('中');
+    //   notif.requestPermission((permission) => {
+    //     console.log('中');
+    //     if (permission === 'granted') {
+    //       console.log('中');
+    //       const notification = new window.Notification('Hi there');
+    //     }
+    //   });
+    // }
   }
 
   // 消える時実行
@@ -116,11 +167,15 @@ class AppChat extends Component {
           <div className="App-header">
             <h2>Chat</h2>
           </div>
-          <div className="MessageList">
+          <List className="MessageList">
             {this.state.messages.map((m) => {
-              return <Message key={m.id} message={m} />;
+              return (
+                <ListItem>
+                  <Message key={m.id} message={m} />
+                </ListItem>
+              );
             })}
-          </div>
+          </List>
           <ChatBox onTextChange={this.onTextChange} onButtonClick={this.onButtonClick} />
           <div className="">
             <Button variant="contained" name="join" color="primary" className="" onClick={this.onJoinButtonClick}>Join</Button>
